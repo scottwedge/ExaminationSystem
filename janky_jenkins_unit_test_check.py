0098fb96baedf0efcc9ea122dd620f8b78457bfd
jdavidaiken <jdavidaiken@gmail.com>
2019-09-05 16:19:48 -0500

    Unittest giving valid responses

--- janky_jenkins_unit_test_check.py
+++ janky_jenkins_unit_test_check.py
@@ -1,7 +1,18 @@
 import sys
+import unittest
 
-param = str(sys.argv[1])
+debug = "yes"
 
-print ("Script name is: ", param)
+class TestStrings(unittest.TestCase):
 
-#p
+    param = str(sys.argv[1])
+
+    print ("Script name is: ", param)
+
+    #Getter: Prompt user for input, return input
+
+    def test_Input(self):
+        print ("Please type yes to answer the question correctly")
+        inp = str(debug)
+        self.assertEqual(inp,"yes")
+        print("You entered: ", inp)
