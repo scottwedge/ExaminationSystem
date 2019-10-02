7bc6eacebe0447a41a9ef10e41f9216940b7982b
Joshua Aiken <jdavidaiken@gmail.com>
2019-10-02 17:30:05 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # test.py
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World #YoloTwerkingSwag big butt hips dont lie Syr Myx-a-lot"] # python3
+    return [b"Hello World #YoloTwerkingSwag big butt hips dont lie Syr Myx-a-lot REMIX"] # python3
     #return ["Hello World"] # python2
