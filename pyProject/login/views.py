af9dd64ba693db330263eefce5153f70603b5ad3
jdavidaiken <jdavidaiken@gmail.com>
2019-10-02 18:25:41 +0000

    Clean-up-clean-up-everybody-do-your-sharels

--- pyProject/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
