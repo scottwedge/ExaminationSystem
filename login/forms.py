2173e50afb74948cd89f51ede9bf047e4c6ff4f0
jdavidaiken <jdavidaiken@gmail.com>
2019-10-12 16:30:31 +0000

    Hey austin, this is a comit message

--- login/forms.py
+++ login/forms.py
@@ -6,7 +6,7 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     firstname = forms.Field(required=True, label='First Name')
     lastname = forms.Field(required=True, label='Last Name')
-
+    
     class Meta:
         model = User
         fields = ['username', 'email','firstname', 'lastname', 'password1', 'password2']
