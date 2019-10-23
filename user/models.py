f7f0cb59d2ebc10669a84a8b9ef2868db0e8cd2d
sbaustin <sbaustin@uno.edu>
2019-10-22 20:57:06 -0500

    Fixed small error

--- user/models.py
+++ user/models.py
@@ -9,6 +9,6 @@ class Profile(models.Model):
     country = models.CharField(default = 'United States', max_length=40, blank = True)
     state = models.CharField(default='Louisiana', max_length=40, blank = True)
     city = models.CharField(default='New Orleans', max_length=40, blank = True)
-    bio = models.CharField(default="Enter a Bio", max_length=200, blank=True)
+    bio = models.CharField(default="Enter a Bio", max_length=200, blank=True, null=True)
     def __str__(self):
         return f'{self.user.username} Profile'
